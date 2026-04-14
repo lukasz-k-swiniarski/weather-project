66f7500565b7cb2730c82f3b33c913989d779c94
lukasz-k-swiniarski <lukasz.k.swiniarski@gmail.com>
2026-04-14 23:12:45 +0200

    Fix minor bug on parser - created sorted list

--- crawler/test_crawler.py
+++ crawler/test_crawler.py
@@ -8,7 +8,6 @@ from column_mapping import load_column_mapping
 if __name__ == "__main__":
 
     encoding = "cp1250"
-    columns_dic_url = "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_t_format.txt"
 
     DATASET_CONFIG = {
         "dataset_1": {
@@ -17,7 +16,8 @@ if __name__ == "__main__":
             "schema": {
                 "source": "url",
                 "path": "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_nag%c5%82%c3%b3wek.csv",
-                "encoding": encoding
+                "encoding": encoding,
+                "col_mapp": "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_format.txt"
             }
         },
         "dataset_2": {
@@ -26,17 +26,29 @@ if __name__ == "__main__":
             "schema": {
                 "source": "url",
                 "path": "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_t_nag%c5%82%c3%b3wek.csv",
-                "encoding": encoding
+                "encoding": encoding,
+                "col_mapp": "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_t_format.txt"
             }
         }
     }
+
+    sorted_dataset_dict = dict(
+        sorted(
+            DATASET_CONFIG.items(),
+            key=lambda x: len(x[1]["prefix"]),
+            reverse=True
+        )
+    )
+
     url = "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/"
+
+
 
     crawler = CrawlerService(url)
     downloader = DownloadService()
     extractor = ZipExtractor()
     schema_loader = SchemaLoader()
-    parser = Parser(DATASET_CONFIG, schema_loader)
+    parser = Parser(sorted_dataset_dict, schema_loader)
 
     all_files = []
     files_dic = crawler.crawl()
@@ -47,10 +59,10 @@ if __name__ == "__main__":
             all_files.extend(files)
 
     dataframes = parser.parse(all_files)
-    print(dataframes)
 
-    col_mapp = load_column_mapping(columns_dic_url)
     for key, df in dataframes.items():
+        col_mapp_url = sorted_dataset_dict[key]["schema"]["col_mapp"]
+        col_mapp = load_column_mapping(col_mapp_url)
         dataframes[key] = df.rename(columns=col_mapp)
         print(dataframes[key])
 
