318bc0b67922aba79ebc78e4407fc91b1a474742
lukasz-k-swiniarski <lukasz.k.swiniarski@gmail.com>
2026-04-24 19:40:08 +0200

    Minor code adjustment

--- crawler/column_mapping.py
+++ crawler/column_mapping.py
@@ -1,19 +1,18 @@
 import requests
+import unicodedata
+
+def remove_diacritics(text: str) -> str:
+    normalized = unicodedata.normalize('NFKD', text)
+    return ''.join(c for c in normalized if not unicodedata.combining(c))
+
 
 def load_column_mapping(url: str) -> dict:
-    """
-    Downloads and parses column mapping from a text file.
-
-    Expected format:
-    CODE    Description
-    NSP     Kod stacji
-    POST    Nazwa stacji
-    """
-
     text = requests.get(url).text
 
     mapping = {}
     for line in text.splitlines():
+        line = remove_diacritics(line)  # 👈 this is the key part
+
         parts = line.strip().split(maxsplit=1)
         if len(parts) == 2:
             key, value = parts
--- crawler/test_crawler.py
+++ crawler/test_crawler.py
@@ -16,33 +16,6 @@ if __name__ == "__main__":
     dataset_config = get_dataset_config()
     dataset_schema_config = get_dataset_schema_config()
 
-    #encoding = "cp1250"
-
-    '''
-    DATASET_CONFIG = {
-        "dataset_1": {
-            "prefix": "s_d_",
-            "encoding": encoding,
-            "schema": {
-                "source": "url",
-                "path": "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_nag%c5%82%c3%b3wek.csv",
-                "encoding": encoding,
-                "col_mapp": "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_format.txt"
-            }
-        },
-        "dataset_2": {
-            "prefix": "s_d_t_",
-            "encoding": encoding,
-            "schema": {
-                "source": "url",
-                "path": "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_t_nag%c5%82%c3%b3wek.csv",
-                "encoding": encoding,
-                "col_mapp": "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/s_d_t_format.txt"
-            }
-        }
-    }
-    '''
-
     sorted_dataset_dict = dict(
         sorted(
             dataset_schema_config.items(),
@@ -50,10 +23,6 @@ if __name__ == "__main__":
             reverse=True
         )
     )
-
-    #url = "https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/"
-
-
 
     crawler = CrawlerService(dataset_config['url'])
     downloader = DownloadService()
@@ -68,16 +37,17 @@ if __name__ == "__main__":
             zip_file_path = downloader.download(file.url)
             files = extractor.extract(zip_file_path)
             all_files.extend(files)
-            break
-        break
+            #break
+        #break
 
     dataframes = parser.parse(all_files)
 
     for key, df in dataframes.items():
         col_mapp_url = sorted_dataset_dict[key]["schema"]["col_mapp"]
-        col_mapp = load_column_mapping(col_mapp_url)
-        dataframes[key] = df.rename(columns=col_mapp)
         db.upload_dataframe(dataframes[key],f'Synop data {key}')
+        #col_mapp = load_column_mapping(col_mapp_url)
+        #dataframes[key] = df.rename(columns=col_mapp)
         print(dataframes[key])
+
 
 
